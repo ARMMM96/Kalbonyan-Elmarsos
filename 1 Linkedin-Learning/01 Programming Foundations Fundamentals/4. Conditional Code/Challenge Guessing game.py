5dbed447090771a7da1a4662a9368fe3a0b84c36
AHMED RABIE MAHMOUD <ahmed.rabie.mahmoud.96@gmail.com>
2022-04-07 08:40:03 +0200

    01 Programming Foundations Fundamentals done

---
+++ 1 Linkedin-Learning/01 Programming Foundations Fundamentals/4. Conditional Code/Challenge Guessing game.py
@@ -0,0 +1,10 @@
+print("Please your favorite food")
+
+userFavoriteFood = input()
+myFavoriteFood = "potato"
+
+if userFavoriteFood == myFavoriteFood:
+    print("Yah I love ",  myFavoriteFood, " too")
+else:
+    print("No i don't like ", userFavoriteFood, " unfortunately")
+
---
+++ 1 Linkedin-Learning/01 Programming Foundations Fundamentals/5. Modular Code/Challenge Favorite cities.py
@@ -0,0 +1,13 @@
+def favorite_city(name):
+    print("One of my favorite cities is", name)
+
+
+favorite_city("Alexandria")
+favorite_city("Sydney")
+favorite_city("chicago illinois")
+
+
+def mult_inv(num):
+  return 1/num
+result=mult_inv(3)
+print(result)
